6c1001107bb54bfde8b0c886a60d50e1c0e337f9
Baasbank Akinmuleya <baasb4real@yahoo.com>
2017-04-10 04:43:14 +0100

    Adds a new test case

--- OOP/spec/phoneSpec.js
+++ OOP/spec/phoneSpec.js
@@ -1,2 +1,26 @@
-let phone = require ('../src/phone.js');
+let Phone = require ('../src/phone');
 
+
+  'use strict';
+  describe("Phone class test", function() {
+    describe("Objects should be of the class", function(){
+
+    it("should be a type of `object`", function() {
+      var phone1 = new Phone();
+      expect(typeof phone1).toEqual(typeof {});
+    });
+    
+    it("should be an instance of the Phone class, function", function() {
+      var phone1 = new Phone();
+      expect(phone1 instanceof Phone).toBeTruthy();
+    });
+
+    it("should have an model, manufacturer, and IMEI that is a type of phone, ", function() {
+      var tecno  = new Phone('P5', 'Tecno', 1609898888);
+      expect(tecno.model).toBe('P5');
+      expect(tecno.manufacturer).toBe('Tecno');
+      expect(tecno.Imei).toBe(1609898888);
+    });
+
+  });
+  });  
